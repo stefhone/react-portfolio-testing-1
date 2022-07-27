dff43df74a4e7a82a17c26f96ef25cef92eca24c
Stefan Hone <stefan.hone@live.co.uk>
2022-07-27 01:43:44 +0100

    Header Component part 2 - HeaderCTA component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import HeaderCTA from './HeaderCTA'
 
 const Header = () => {
   return (
@@ -8,6 +9,7 @@ const Header = () => {
         <h5>Hello, I'm</h5>
         <h1>Stefan Hone</h1>
         <h5 className="text-light">Front End Developer</h5>
+        <HeaderCTA/>
       </div>
     </header>
   )
