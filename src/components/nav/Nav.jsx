d0fdd1bd9586cfdf4156026a5478dc24b59cbc42
Stefan Hone <stefan.hone@live.co.uk>
2022-07-29 23:24:33 +0100

    Nav setActiveNav state logic on each nav link

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,11 +13,11 @@ const Nav = () => {
 
   return (
     <nav>
-      <a href="#" className='active'><RiHome2Line/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><TbBook/></a>
-      <a href="#services"><RiServiceLine/></a>
-      <a href="#contact"><TbMessages/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><RiHome2Line/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><TbBook/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><TbMessages/></a>
     </nav>
   )
 }
