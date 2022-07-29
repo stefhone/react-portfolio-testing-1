1bf6cef2f0ca0edf6115f6b14ace7e111e4a7949
Stefan Hone <stefan.hone@live.co.uk>
2022-07-29 01:46:14 +0100

    Nav Component setup with nav links and icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-import './nav.css'
+import { useState } from 'react';
+import './nav.css';
+import {RiHome2Line} from 'react-icons/ri'
+import {AiOutlineUser} from 'react-icons/ai'
+import {TbBook} from 'react-icons/tb'
+import {RiServiceLine} from 'react-icons/ri'
+import {TbMessages} from 'react-icons/tb'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><RiHome2Line/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><TbBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contact"><TbMessages/></a>
+    </nav>
   )
 }
 
