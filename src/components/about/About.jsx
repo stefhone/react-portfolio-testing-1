b81cc4bb7c1c2d9d015fa2939eda89a5429ac499
Stefan Hone <stefan.hone@live.co.uk>
2022-08-01 00:18:44 +0100

    About Component part 1 - initial jsx/html code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
     <section id="about">
-      About
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Stefan Hone" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>10+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>10+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+          
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Corporis accusantium, at quia, sunt inventore atque similique mollitia, laudantium dolore facilis aspernatur. 
+            Tempore distinctio quidem architecto sunt consequatur nulla corporis quas.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's talk</a>
+        </div>
+      </div>     
     </section>
   )
 }
