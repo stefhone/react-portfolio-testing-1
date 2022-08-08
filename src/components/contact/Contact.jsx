8dbd3a7624fe90559018f97c493f84bbe873e7be
Stefan Hone <stefan.hone@live.co.uk>
2022-08-08 19:28:44 +0100

    Contact Component P1 - Initial jsx/html code

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
     <section id="contact">
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>stefan.hone@live.co.uk</h5>
+            <a href="mailto:stefan.hone@live.co.uk" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>idspiral</h5>
+            <a href="https://m.me/idspiral" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+447528638448</h5>
+            <a href="https://api.whatsapp.com/send?phone=447528638448" target="_blank" rel="noreferrer">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+
+        </form>
+      </div>
     </section>
   )
 }
